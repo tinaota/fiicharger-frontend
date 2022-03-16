283b6b2f1c08cf704e6db7cb24f6b1e3250b269b
jocelyn <jocelyn@dd0af81a-71fc-1848-8729-91611fed4ea5>
2022-03-16 08:24:33 +0000

    feat: update version to 2.3.0
    git-svn-id: https://210.61.29.146:88/svn/FiiEVCharging/web@309 dd0af81a-71fc-1848-8729-91611fed4ea5

--- src/utils/global.js
+++ src/utils/global.js
@@ -1,6 +1,6 @@
 export const $GLOBAL_PROJECT = 'FiiCharger';
 export const $GLOBAL_HTTP = '/EV_Charging_Web'; //EV_Charging_Web EV_Charging_Web_test
-export const $GLOBAL_VERSION = '2.2.3';
+export const $GLOBAL_VERSION = '2.3.0';
 export const $GLOBAL_LANG = {
     "en": "English",
     "zh-tw": "繁體中文",
