5bf0b3f8776706e4d240cfcfe79be67c5f3a1503
kshitij.lingthep <kshitij.lingthep@fii-usa.com>
2022-04-27 14:07:09 -0500

    feat: add delete vehicle component

---
+++ src/views/setting/deleteCars.vue
@@ -0,0 +1,97 @@
+<template>
+    <el-dialog :title="$t('general.delete')" width="420px" :visible.sync="visible" custom-class="" :show-close="false" v-loading="isLoading" @close="closeDialog()">
+        <div class="formVertical">
+            <div class="form-item">
+                Are you sure you want to delete this vehicle?
+                <h3>Info</h3>
+                <p> Id: {{id}}</p>
+                <p> Make: {{make}}</p>
+                <p> Model:{{carModel}}</p>
+                <p> Trim: {{trim}}</p>
+            </div>
+        </div>
+        <span slot="footer" class="dialog-footer">
+            <el-button size="small" @click="isUpdate = false; visible = false;">{{ $t('general.cancel') }}</el-button>
+            <el-button size="small" type="primary" @click="deleteCars">{{ $t('general.ok') }}</el-button>
+        </span>
+    </el-dialog>
+</template>
+
+<script>
+import { $HTTP_deleteVehicle } from "@/api/api";
+
+import { setScrollBar } from "@/utils/function";
+export default {
+    props: {
+        id: Number,
+        make: String,
+        carModel: String,
+        trim: String,
+        show: Boolean,
+    },
+    data() {
+        return {
+            lang: "",
+            visible: false,
+            isLoading: false,
+            isUpdate: false,
+            $API: null,
+        };
+    },
+    created() {
+        this.lang = window.sessionStorage.getItem("fiics-lang");
+    },
+    watch: {
+        show: {
+            deep: true,
+            handler() {
+                const that = this;
+                that.visible = that.show;
+                that.isUpdate = false;
+                if (that.visible) {
+                    this.$Api = $HTTP_deleteVehicle;
+                }
+                that.$jQuery(".formVertical").length > 0 && this.$jQuery(".formVertical").mCustomScrollbar("destroy");
+                that.$nextTick(() => {
+                    setScrollBar(".formVertical", that);
+                });
+            },
+        },
+    },
+    methods: {
+        deleteCars() {
+            const that = this;
+            let params = {
+                id: that.id,
+            };
+
+            that.isLoading = true;
+            this.$Api(params)
+                .then((data) => {
+                    that.isLoading = false;
+                    if (data.id === params.id) {
+                        that.$message({ type: "success", message: i18n.t("general.sucUpdateMsg") });
+                        that.isUpdate = true;
+                        that.visible = false;
+                    }
+                })
+                .catch((err) => {
+                    console.log(err);
+                    let _errors = err?.data?.errors ? Object.values(err?.data?.errors) : err?.data;
+                    that.$message({ type: "warning", message: _errors.toString() });
+                });
+        },
+        closeDialog() {
+            this.$emit("close", this.isUpdate);
+        },
+    },
+};
+</script>
+<style lang = "scss" scoped>
+.formVertical {
+    min-height: 200px;
+    div {
+        min-height: 200px;
+    }
+}
+</style>
