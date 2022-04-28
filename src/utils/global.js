5508c3dc671ddb6df0c61960bf24e3e80d078331
kshitij.lingthep <kshitij.lingthep@fii-usa.com>
2022-04-28 14:49:59 -0500

    refactor: change base path if it is not development

--- src/utils/global.js
+++ src/utils/global.js
@@ -1,7 +1,10 @@
+let environment = process.env.NODE_ENV
+let base_url = process.env.VUE_APP_BASE_LOGIN_URL
+
 export const $GLOBAL_PROJECT = "FiiCharger";
 export const $GLOBAL_HTTP = "/EV_Charging_Web"; //EV_Charging_Web EV_Charging_Web_test
-export const $GLOBAL_AUTH = "Gatekeeper";
-export const $GLOBAL_VEHICLE = "Vehicle";
+export const $GLOBAL_AUTH = environment === 'development' ? "Gatekeeper" : base_url + '/Gatekeeper';
+export const $GLOBAL_VEHICLE = environment === 'development' ? "Vehicle" : base_url + '/Vehicle';
 export const $GLOBAL_VERSION = "2.3.0";
 export const $GLOBAL_LANG = {
     "en": "English",
