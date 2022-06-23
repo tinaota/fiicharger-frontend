075e6c853b79ac3e0cc5df099ffe42f6f81dd02d
kshitij.lingthep <kshitij.lingthep@fii-usa.com>
2022-06-23 09:03:24 -0500

    refactor: reorder the language selection list

--- src/utils/global.js
+++ src/utils/global.js
@@ -11,10 +11,10 @@ export const $GLOBAL_CLIENT_ID = "7c13221fcef3e92495d6ed610023accb.webapp.fii-us
 export const $GLOBAL_VERSION = "2.3.0";
 export const $GLOBAL_LANG = {
     "en": "English",
-    "zh-tw": "繁體中文",
+    "es": "Español",
     "zh-cn": "简体中文",
-    "vi": "Tiếng Việt",
-    "es": "español"
+    "zh-tw": "繁體中文",
+    "vi": "Tiếng Việt"
 };
 export const $GLOBAL_CURRENCY = {
     1: "$",
