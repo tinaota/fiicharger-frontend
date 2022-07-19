09684be4fe2a20dc1fa4c15f6defb19ca471806e
kshitij.lingthep <kshitij.lingthep@fii-usa.com>
2022-07-19 14:22:24 -0500

    feat: add pagination in transaction

--- src/components/chargingStation/transaction.vue
+++ src/components/chargingStation/transaction.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <div class="filter">
-            <el-select class="select-small" v-model="filter.stopReason" placeholder="Stop Reason" v-loading="allTransactionsReasonList.isLoading" @change="getAllTransactions()" clearable>
+            <el-select class="select-small" v-model="filter.stopReason" placeholder="Stop Reason" v-loading="allTransactionsReasonList.isLoading" @change="getAllTransactions('filter')" clearable>
                 <el-option v-for="item in allTransactionsReasonList.data" :label="item" :key="item" :value="item"></el-option>
             </el-select>
         </div>
@@ -13,25 +13,28 @@
             <!-- <el-table-column prop="reservationId" label="Reservation Id" :min-width="2"></el-table-column> -->
             <el-table-column label="Start Time Stamp" :min-width="2">
                 <template slot-scope="scope">
-                    {{getLocTime(scope.row.startTimestamp)}}
+                    {{ getLocTime(scope.row.startTimestamp) }}
                 </template>
             </el-table-column>
             <el-table-column prop="meterStop" label="Meter Stop" :min-width="2"></el-table-column>
             <el-table-column label="Stop Time Stamp" :min-width="2">
                 <template slot-scope="scope">
-                    {{scope.row.stopTimestamp!==null? getLocTime(scope.row.stopTimestamp):''}}
+                    {{ scope.row.stopTimestamp!==null? getLocTime(scope.row.stopTimestamp):'' }}
                 </template>
             </el-table-column>
             <el-table-column prop="stopReason" label="Stop Reason" :min-width="2"></el-table-column>
             <el-table-column prop="meterTotal" label="Meter Total" :min-width="2"></el-table-column>
-
         </el-table>
+        <div class="total">{{ $t("general.result", {item:total}) }}</div>
+        <el-pagination background layout="prev, pager, next" :total="total" :pager-count="5" :page-size="limit" :current-page.sync="page" @current-change="changePage">
+        </el-pagination>
     </div>
 </template>
-    
+
     <script>
 import { transformUtcToLocTime } from "@/utils/function";
 import { $HTTP_getAllTransactionsReasonList, $HTTP_getAllTransactions } from "@/api/api";
+import { $GLOBAL_PAGE_LIMIT } from "@/utils/global";
 
 export default {
     props: {
@@ -48,6 +51,9 @@ export default {
                 data: [],
                 isLoading: false,
             },
+            total:0,
+            limit: $GLOBAL_PAGE_LIMIT,
+            page:1
         };
     },
     computed: {
@@ -60,22 +66,39 @@ export default {
         this.getTransactionsReasonList();
     },
     methods: {
-        getAllTransactions() {
+        getAllTransactions(type) {
             this.isLoading = true;
-            let params = {};
+            let params = {
+                page: this.page,
+                limit: this.limit,
+            };
             if (this.filter.stopReason) {
                 params.StopReason = this.filter.stopReason;
             }
             if (this.chargerId) {
                 params.ChargePointId = this.chargerId;
             }
+            if(type==="filter"){
+                this.page=1;
+                params.page=1;
+            }
             $HTTP_getAllTransactions(params)
                 .then((res) => {
-                    this.allTransactions = res.data;
                     this.isLoading = false;
+                    if (res?.data?.length > 0) {
+                        this.allTransactions = res.data;
+                        this.total = res.metadata.totalRows;
+                    } else {
+                        this.allTransactions = [];
+                        this.total = 0;
+                        this.$message({ type: "warning", message: i18n.t("emptyMessage") });
+                    }
                 })
                 .catch((err) => {
+                    this.allTransactions=[]
+                    this.total=0
                     console.log(err);
+                    this.$message({ type: "warning", message: i18n.t("error_network") });
                 });
         },
         getTransactionsReasonList() {
@@ -89,7 +112,29 @@ export default {
                     console.log(err);
                 });
         },
+        changePage(page) {
+            this.page = page;
+            this.getAllTransactions();
+        },
     },
 };
 </script>
-    
+
+<style lang = "scss" scoped>
+.mainctrl .card-8 {
+    padding: 28px;
+    width: calc(100% - 56px);
+    position: relative;
+    .total {
+        position: absolute;
+        right: 54px;
+        bottom: 66px;
+        height: 36px;
+        line-height: 36px;
+        vertical-align: middle;
+        font-size: 1rem;
+        color: #5a607f;
+        letter-spacing: 0;
+    }
+}
+</style>
