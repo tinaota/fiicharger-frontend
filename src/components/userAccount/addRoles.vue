8761fed7e903459715a65d94c1fe0dbb80774e95
kshitij.lingthep <kshitij.lingthep@fii-usa.com>
2022-08-01 11:37:39 -0500

    refactor: remove vue-multiselect package-lock
    	    use element ui multi select

--- src/components/userAccount/addRoles.vue
+++ src/components/userAccount/addRoles.vue
@@ -2,13 +2,15 @@
     <el-dialog :title="$t('general.addRoles')" width="400px" :visible.sync="visible" custom-class="" :show-close="false" v-loading="isLoading" @close="closeDialog()">
         <div class="formVertical">
             <div class="form-item">
-                {{$t('general.updateRolesUser')}}
+                {{ $t('general.updateRolesUser') }}
             </div>
 
             <div class="roles-multi-select">
-                <multiselect v-model="value" :options="options" multiple :show-labels="false"></multiselect>
-                <p> {{$t('general.changeRoles')}}
-                    {{value}}</p>
+                <el-select class="autoresizeselect" v-model="value" clearable multiple filterable allow-create default-first-option>
+                    <el-option v-for="item in options" :label="item" :key="item" :value="item"></el-option>
+                </el-select>
+                <p> {{ $t('general.changeRoles') }}
+                    {{ value }}</p>
             </div>
 
         </div>
@@ -22,14 +24,12 @@
 <script>
 import { $HTTP_getRoles, $HTTP_addRoles } from "@/api/api";
 import { setScrollBar } from "@/utils/function";
-import Multiselect from "vue-multiselect";
 
 export default {
     props: {
         id: String,
-        show: Boolean,
+        show: Boolean
     },
-    components: { Multiselect },
 
     data() {
         return {
@@ -38,7 +38,7 @@ export default {
             isUpdate: false,
             $API: null,
             value: null,
-            options: ["Admin", "Owner"],
+            options: ["Admin", "Owner"]
         };
     },
     watch: {
@@ -51,23 +51,29 @@ export default {
                 if (that.visible) {
                     this.$Api = $HTTP_addRoles;
                     let params = {
-                        id: this.id,
+                        id: this.id
                     };
                     $HTTP_getRoles(params)
                         .then((res) => {
                             that.value = res;
                         })
                         .catch((err) => {
-                            let _errors = err?.data?.errors ? Object.values(err?.data?.errors) : err?.data;
-                            that.$message({ type: "warning", message: _errors.toString() });
+                            let _errors = err?.data?.errors
+                                ? Object.values(err?.data?.errors)
+                                : err?.data;
+                            that.$message({
+                                type: "warning",
+                                message: _errors.toString()
+                            });
                         });
                 }
-                that.$jQuery(".formVertical").length > 0 && this.$jQuery(".formVertical").mCustomScrollbar("destroy");
+                that.$jQuery(".formVertical").length > 0 &&
+                    this.$jQuery(".formVertical").mCustomScrollbar("destroy");
                 that.$nextTick(() => {
                     setScrollBar(".formVertical", that);
                 });
-            },
-        },
+            }
+        }
     },
     beforeDestroy() {},
     methods: {
@@ -75,7 +81,7 @@ export default {
             const that = this;
             let params = {
                 id: that.id,
-                roles: that.value,
+                roles: that.value
             };
 
             that.isLoading = true;
@@ -84,20 +90,28 @@ export default {
                 .then((data) => {
                     that.isLoading = false;
                     if (data.succeeded) {
-                        that.$message({ type: "success", message: i18n.t("general.sucUpdateMsg") });
+                        that.$message({
+                            type: "success",
+                            message: i18n.t("general.sucUpdateMsg")
+                        });
                         that.isUpdate = true;
                         that.visible = false;
                     }
                 })
                 .catch((err) => {
-                    let _errors = err?.data?.errors ? Object.values(err?.data?.errors) : err?.data;
-                    that.$message({ type: "warning", message: _errors.toString() });
+                    let _errors = err?.data?.errors
+                        ? Object.values(err?.data?.errors)
+                        : err?.data;
+                    that.$message({
+                        type: "warning",
+                        message: _errors.toString()
+                    });
                 });
         },
         closeDialog() {
             this.$emit("close", this.isUpdate);
-        },
-    },
+        }
+    }
 };
 </script>
 <style lang = "scss" scoped>
@@ -109,9 +123,4 @@ export default {
     height: 150px;
     margin-top: 5px;
 }
-
-.multiselect__option--selected.multiselect__option--highlight {
-    background: blue;
-}
 </style>
-<style src="vue-multiselect/dist/vue-multiselect.min.css"></style>
